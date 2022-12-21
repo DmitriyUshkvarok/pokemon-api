cd5961a09e631d609ae9660db2704f67c462670c
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-12-21 16:52:15 +0200

    ...

--- src/main.js/full-collection-api.js
+++ src/main.js/full-collection-api.js
@@ -1,14 +1,49 @@
 import axios from 'axios';
-import cardPokemon from '../templates/card-pokemon.hbs';
+import resultApi from '../templates/results-api.hbs';
 import { refs } from './render-api';
+// import InfiniteScroll from 'infinite-scroll';
+
+async function getFullInfo(urlArray) {
+  const results = await Promise.all(urlArray);
+  return results;
+}
+let page = '';
 
 function fullFetch() {
   return axios
-    .get('https://pokeapi.co/api/v2/ability/?limit=20&offset=20')
-    .then(response => response.data)
-    .then(fullCollection => {
-      const markupColection = cardPokemon(fullCollection);
+    .get(`https://pokeapi.co/api/v2/pokemon-form/?limit=50&offset=${page}`)
+    .then(response => response.data.results)
+    .then(async fullCollection => {
+      const urls = fullCollection.map(({ url }) => url);
+      const data = await getFullInfo(
+        urls.map(el => axios.get(el).then(response => response.data))
+      );
+      page = 1;
+      const markupColection = resultApi({ results: data });
+      refs.gallery.classList.add('show');
       refs.gallery.innerHTML = markupColection;
+
+      if (!data) {
+        refs.gallery.classList.remove('show');
+      }
     });
 }
 fullFetch();
+
+const options = {
+  root: null,
+  rootMargin: '100px',
+  threshold: 0.5,
+};
+const observer = new IntersectionObserver(onLoadPokemonCards, options);
+const target = document.querySelector('.target');
+
+function onLoadPokemonCards(entries) {
+  entries.forEach(entry => {
+    page += 1;
+    if (entry.isIntersecting) {
+      // как сюда передать гет запрос который связан с promise.all что бы реализовать бесконечный скролл
+    }
+  });
+}
+observer.observe(document.querySelector('.target'));
--- src/main.js/render-api.js
+++ src/main.js/render-api.js
@@ -2,8 +2,6 @@ import pokemonCard from '../templates/card-pokemon.hbs';
 import { fetchPokemonApi } from './fetch-pokemon';
 import { Notify } from 'notiflix';
 import { onResetSearch } from './reset-input';
-import listPokemon from '../templates/list-pokemon.hbs';
-import debounce from 'lodash.debounce';
 
 export const refs = {
   searchForm: document.querySelector('.header-search-form'),
@@ -11,6 +9,7 @@ export const refs = {
   gallery: document.querySelector('.gallery'),
   listItem: document.querySelector('.list-markup'),
   searchInput: document.querySelector('.search-input'),
+  cardAll: document.querySelector('.card-all'),
 };
 
 refs.searchForm.addEventListener('submit', onFormSubmit);
@@ -22,6 +21,10 @@ function onFormSubmit(e) {
   if (!inputSearch) {
     onResetSearch();
     return Notify.info('enter the name or id of the pokemon');
+  }
+  if (inputSearch) {
+    refs.gallery.innerHTML = '';
+    refs.gallery.classList.remove('show');
   }
 
   fetchPokemonApi(inputSearch).then(onShowGallery).catch(onError);
